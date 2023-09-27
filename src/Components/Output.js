90ccca28e90cb5adbd28dbdecb927114604b0073
codyivy0 <codyivy0@gmail.com>
2023-09-27 15:46:13 -0500

    bug fixing

--- src/Components/Output.js
+++ src/Components/Output.js
@@ -1,13 +1,13 @@
 export default function Output({ bill, percentA, percentB }) {
     const totalPercent = (parseFloat(percentA) + parseFloat(percentB)) / 2;
     const totalDecimal = totalPercent / 100
-    const totalTipAmount = bill * totalDecimal
-    const total = parseInt(bill) + totalTipAmount
+    const totalTipAmount = +(bill * totalDecimal).toFixed(2)
+    const total = (parseInt(bill) + totalTipAmount).toFixed(2);
 
 
   return (
     <>
-      <h1>You pay ${total} (${bill} + ${totalTipAmount} tip)</h1>
+      <h1>You pay ${total} (${bill} + ${totalTipAmount.toFixed(2)} tip)</h1>
     </>
   );
 }
